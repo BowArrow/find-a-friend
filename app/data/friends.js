13423c4282e8dbd63b8bb67530340b7fdbc63f58
nBow <nbow51@gmail.com>
2019-01-05 19:45:56 -0800

    almost done

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+
+var friendsArray = [
+    {
+        name: "Casey",
+        photo: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/22008340_10155769562823151_7107849493718746789_n.jpg?_nc_cat=104&_nc_ht=scontent-sjc3-1.xx&oh=4b538f877c76ddbc0c43187fe4248cd4&oe=5CC1ED06",
+        scores: [
+            5,
+            5,
+            5,
+            4,
+            2,
+            5,
+            4,
+            4,
+            2,
+            4
+        ]
+    },
+    {
+        name: "POPOJIJO",
+        photo: "https://i.pinimg.com/originals/01/9f/52/019f52a3b00dfd8c633d80e65a64ab08.jpg",
+        scores: [
+            2,
+            3,
+            1,
+            3,
+            4,
+            5,
+            2,
+            3,
+            1,
+            4
+        ]
+    }
+]
+
+module.exports = friendsArray;
